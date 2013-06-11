a438fd45c435b21162c2b5a092b6a1ffb6cb2ec8
Brady <bradylill@gmail.com>
2013-06-11 21:45:37 +1000

    Setting tile size for the map.

--- src/desolate/game.clj
+++ src/desolate/game.clj
@@ -2,22 +2,21 @@
 
 (defrecord Pos [x y z])
 
-(def walk-speed 5)
-(def run-speed 10)
-(def tile-size 5)
+(def walk-speed 1)
+(def run-speed 2)
 
 (def player { :sprite "@" :parts []})
 (def rock   { :sprite "*" :parts []})
 (def ship   { :sprite "#" 
-              :parts [{ :sprite "=" :pos (Pos. -5 0 0) }
-                      { :sprite "=" :pos (Pos. 5 0 0)  }
-                      { :sprite ">" :pos (Pos. 10 0 0) }
-                      { :sprite "=" :pos (Pos. -5 -5 0)}
-                      { :sprite "=" :pos (Pos. 0 -5 0) }
-                      { :sprite ">" :pos (Pos. 5 -5 0) }
-                      { :sprite "=" :pos (Pos. -5 5 0) }
-                      { :sprite "=" :pos (Pos. 0 5 0)  }
-                      { :sprite ">" :pos (Pos. 5 5 0)  }]})
+              :parts [{ :sprite "=" :pos (Pos. -1 0 0) }
+                      { :sprite "=" :pos (Pos. 1 0 0)  }
+                      { :sprite ">" :pos (Pos. 2 0 0) }
+                      { :sprite "=" :pos (Pos. -1 -1 0)}
+                      { :sprite "=" :pos (Pos. 0 -1 0) }
+                      { :sprite ">" :pos (Pos. 1 -1 0) }
+                      { :sprite "=" :pos (Pos. -1 1 0) }
+                      { :sprite "=" :pos (Pos. 0 1 0)  }
+                      { :sprite ">" :pos (Pos. 1 1 0)  }]})
 
 (def world (atom { :objects [{:type player :pos (Pos. 50000 10000 0)}
                              {:type ship   :pos (Pos. 49980 10000 0)}
--- src/desolate/gfx.clj
+++ src/desolate/gfx.clj
@@ -4,23 +4,30 @@
             [desolate.input :as input])
   (:import  [desolate.game Pos]))
 
+(def tile-size 5)
+
 (defn setup []
   (q/smooth)
   (q/frame-rate 30)
   (q/background 200))
 
+(defn- part-coordinate [coord part reference]
+  (+ (coord reference) 
+     (* (get-in part [:pos coord])
+        tile-size)))
+
 (defn- draw-part [part reference-pos]
-  (let [x (+ (:x reference-pos)
-             (get-in part [:pos :x]))
-        y (+ (:y reference-pos)
-             (get-in part [:pos :y]))]
+  (let [x (part-coordinate :x part reference-pos)
+        y (part-coordinate :y part reference-pos)]
     (q/text (:sprite part) x y)))
 
+(defn- object-coordinate [coord object current-pos]
+  (* (- (get-in object [:pos coord]) (coord current-pos))
+     tile-size))
+
 (defn- draw-object [object current-pos]
-  (let [x (- (get-in object [:pos :x])
-             (:x current-pos))
-        y (- (get-in object [:pos :y])
-             (:y current-pos))]
+  (let [x (object-coordinate :x object current-pos) 
+        y (object-coordinate :y object current-pos)]
     (q/text (get-in object [:type :sprite]) x y)
     (doseq [part (get-in object [:type :parts])]
       (draw-part part (Pos. x y 0)))))
@@ -30,8 +37,8 @@
     (draw-object object current-pos)))
 
 (defn draw []
-  (q/background 200)
-  (draw-world @game/world (Pos. 49839 9900 0)))
+  (q/background 55)
+  (draw-world @game/world (Pos. 49939 9990 0)))
 
 (defn start-rendering []
   (q/defsketch desolate
@@ -39,4 +46,4 @@
     :key-typed input/key-press
     :setup setup
     :draw draw
-    :size [323 200]))
+    :size [800 600]))
